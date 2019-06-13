268d9791d3da5bd22903871295ece91b6b7eb5bd
Han Seoulo <cu.rogue.coder@gmail.com>
2019-06-13 18:53:50 -0400

    Initialized my first React App

---
+++ clickity-clicky/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Navi from './component/navi/navi'
+
+function App() {
+  return (
+    <Navi />
+  );
+}
+
+export default App;
---
+++ clickity-clicky/src/component/navi/navi.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Navi () {
+    return (
+      <nav className="navbar navbar-dark bg-dark">
+        <a className="navbar-brand" href="#">Columbia BootCamp Class Roster</a>
+      </nav>
+    );
+}
+
+export default Navi;
+
